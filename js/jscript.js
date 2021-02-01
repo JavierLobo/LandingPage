078897f78ed6454528249cd072b699b19bfbf2e4
Javixu <franciscoJavier.Lobo@gmail.com>
2021-02-01 13:28:46 +0100

    (DEV) Clear and update files. Close #20
    Reinicio del portafolio, se eliminan todos los archivos dejando solo los esenciales.

--- js/jscript.js
+++
@@ -1,9 +0,0 @@
-function body_onLoad() {
-	// Una vez que se haya cargado la página, se ejecuta este evento
-}
-
-function body_onUnLoad() {
-	// Una vez que se haya descargado la página, se ejecuta este evento
-}
-
-// console.log("");
